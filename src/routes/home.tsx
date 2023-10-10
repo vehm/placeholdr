320127c4737779a02526d246fdcf6e8f997cbba9
Vehm <github@vehm.dev>
2023-10-10 01:44:59 -0500

    Set Up Page on Home Route

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,9 @@
+import Page from "/@components/Page";
+
 const Home = () => {
-	return <>Home</>;
+	const routeTitle = "Home | Placeholdr";
+
+	return <Page title={routeTitle}>Home</Page>;
 };
 
 export default Home;
