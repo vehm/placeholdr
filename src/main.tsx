75ec9c59a78a8a7c8443ce7c05d2fa1cdacb714d
Vehm <github@vehm.dev>
2023-10-10 01:17:42 -0500

    Created Router Object and Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import {
+	createBrowserRouter,
+	RouteObject,
+	RouterProvider,
+} from "react-router-dom";
+
+const routes: RouteObject[] = [
+	{
+		path: "/",
+		element: <>Root</>,
+		children: [
+			{
+				index: true,
+				element: <>Home</>,
+			},
+		],
+	},
+];
+
+const router = createBrowserRouter(routes, {
+	future: {
+		v7_normalizeFormMethod: true,
+	},
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode></React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
