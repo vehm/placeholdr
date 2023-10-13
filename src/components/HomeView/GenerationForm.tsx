8a758e23a1b62f57860eb88b5f2254cb6b064b63
Vehm <github@vehm.dev>
2023-10-12 21:29:39 -0500

    Implemented Textarea Limit

--- src/components/HomeView/GenerationForm.tsx
+++ src/components/HomeView/GenerationForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import SessionCard from "./SessionCard";
+import LimitedTextarea from "./LimitedTextarea";
 
 import { GenerateRequest } from "/@types/generate";
 
@@ -45,16 +46,14 @@ const GenerationForm = () => {
 				/>
 			) : (
 				<>
-					<textarea
-						className={"border rounded-lg p-4"}
-						placeholder="Describe the placeholder images of your wildest dreams..."
-						cols={30}
-						rows={5}
+					<LimitedTextarea
+						limit={2000}
 						value={request.prompt}
-						onChange={e =>
-							setRequest({ ...request, prompt: e.target.value })
+						onChange={value =>
+							setRequest({ ...request, prompt: value })
 						}
-					></textarea>
+						placeholder="Describe the placeholder images of your wildest dreams..."
+					/>
 					<button
 						className="btn bg-pink-600 enabled:hover:bg-pink-700 disabled:bg-gray-500 text-white"
 						disabled={!readyToGenerate || isLoading}
